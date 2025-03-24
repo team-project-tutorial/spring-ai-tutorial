91f34cdaa97a3eede6d39b97cfd147354b9b4702
choi-ys <project.log.062@gmail.com>
2025-03-25 02:35:29 +0900

    init: initial project

---
+++ src/main/java/io/sparta/springaitutorial/SpringAiTutorialApplication.java
@@ -0,0 +1,13 @@
+package io.sparta.springaitutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sparta/springaitutorial/SpringAiTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sparta.springaitutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
